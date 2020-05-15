489df6ac07366ceccf62e276c9ff22d5f3bd7845
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-05-15 09:59:03 -0500

    Done.

--- spec/figures_controller_view_spec.rb
+++ spec/figures_controller_view_spec.rb
@@ -120,7 +120,6 @@ describe FiguresController do
     fill_in :figure_name, with: "Missy"
     fill_in :new_landmark, with: "Big Tower"
     click_button "Edit Figure"
-
     expect(page.current_path).to eq("/figures/#{@original_figure.id}")
     expect(page.body).to include("Missy")
     expect(page.body).to include("Big Tower")
