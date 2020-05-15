53d0ddc79959efe5fb8a2a67b3c43b114f214192
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-05-15 10:00:55 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,7 +13,6 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    # binding.pry
     @figure = Figure.create(params[:figure])
     if !params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
